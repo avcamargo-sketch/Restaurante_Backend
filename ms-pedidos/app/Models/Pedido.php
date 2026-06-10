38930d643c1c395c4eba713cde213745c900f106
valentina camargo <avcamargo@uniboyaca.co>
2026-06-10 11:43:34 -0500

    Valentina[Pequeños cambios]

--- ms-pedidos/app/Models/Pedido.php
+++ ms-pedidos/app/Models/Pedido.php
@@ -7,6 +7,6 @@ class Pedido extends Model {
     protected $table = 'pedidos';
     public $timestamps = true;
     
-    // Permitir asignación masiva de estos campos
+  
     protected $fillable = ['mesa_id', 'fecha', 'hora', 'subtotal', 'total', 'cantidad_total', 'estado'];
 }
